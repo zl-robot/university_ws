277694751adcf2bd6baec805a9dc6244728e5a69
zhanglei <zhanglei.20082006@163.com>
2025-03-03 14:51:13 +0800

    parameter commit

--- src/institute_ellite/institute_ellite/zhangsan.py
+++ src/institute_ellite/institute_ellite/zhangsan.py
@@ -2,13 +2,17 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from std_msgs.msg import UInt32
+from institute_interfaces.msg import Homework
 
 class TeacherNode(Node):
     def __init__(self, name):
         super().__init__(name)
         self.get_logger().info("我是%s,是一名老师！" %name)
 
-        self.pub_hw = self.create_publisher(String,"homework",10)
+        self.content = "The Application of Mobile Robots"
+        self.pub_hw = self.create_publisher(Homework,"homework",10)
+
+
         self.i = 1
         self.timer_period = 2
         self.timer = self.create_timer(self.timer_period,self.timer_callback)
@@ -16,14 +20,15 @@ class TeacherNode(Node):
         self.substudentID = self.create_subscription(UInt32,"studentID",self.recv_studentID_callback,10)
 
     def timer_callback(self):
-        msg = String()
-        msg.data = '第%d次作业已发布,请查收。' % (self.i)
+        msg = Homework()
+        msg.content = self.content
+        msg.no = self.i
         self.pub_hw.publish(msg)
-        self.get_logger().info('同学们，我发布了作业："%s"' % msg.data)
+        self.get_logger().info('同学们，我发布了第%d次作业作业：课程名称："%s",' % (msg.no, msg.content))
         self.i += 1
 
     def recv_studentID_callback(self,studengID):
-        self.get_logger().info('学号：%d已经收到了作业' % studengID.data)
+        self.get_logger().info('学号：%d 的同学已经收到了作业' % studengID.data)
 
 
 def main(args=None):
--- src/institute_mechanical/src/lisi.cpp
+++ src/institute_mechanical/src/lisi.cpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/u_int32.hpp"
+#include "institute_interfaces/msg/homework.hpp"
 
 using std::placeholders::_1;
 
@@ -11,22 +12,25 @@ public:
     {
         RCLCPP_INFO(this->get_logger(),"我是学生%s",name.c_str());
 
-        sub_hw = this->create_subscription<std_msgs::msg::String>("homework",10,std::bind(&StudentNode::topic_callback,this,_1));
+        sub_hw = this->create_subscription<institute_interfaces::msg::Homework>("homework",10,std::bind(&StudentNode::topic_callback,this,_1));
+        
         pub_No = this->create_publisher<std_msgs::msg::UInt32>("studentID",10);
     }
 
 private:
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_hw;
+    
+    rclcpp::Subscription<institute_interfaces::msg::Homework>::SharedPtr sub_hw;
     rclcpp::Publisher<std_msgs::msg::UInt32>::SharedPtr pub_No;
 
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+    void topic_callback(const institute_interfaces::msg::Homework::SharedPtr msg)
     {
-        RCLCPP_INFO(this->get_logger(),"收到：'%s'",msg->data.c_str());
+        
+        RCLCPP_INFO(this->get_logger(),"收到课程：'%s'的第%d次作业",msg->content.c_str(),msg->no);
 
         std_msgs::msg::UInt32 studengid;
         studengid.data = 10;
         pub_No->publish(studengid);
-        RCLCPP_INFO(this->get_logger(),"学号：%d,收到：'%s'",studengid.data,msg->data.c_str());
+        RCLCPP_INFO(this->get_logger(),"学号：%d,收到课程：'%s'的第%d次作业",studengid.data, msg->content.c_str(), msg->no);
 
     };
 
