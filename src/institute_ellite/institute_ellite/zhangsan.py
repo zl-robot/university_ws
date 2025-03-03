aab318a95c8cedcbd7b8a9978281642d5dc53367
zhanglei <zhanglei.20082006@163.com>
2025-03-03 16:15:37 +0800

    para commit

--- src/institute_ellite/institute_ellite/zhangsan.py
+++ src/institute_ellite/institute_ellite/zhangsan.py
@@ -10,10 +10,12 @@ class TeacherNode(Node):
 
         self.pub_hw = self.create_publisher(String,"homework",10)
         self.i = 1
-        self.timer_period = 2
+        self.timer_period = 5
         self.timer = self.create_timer(self.timer_period,self.timer_callback)
 
         self.substudentID = self.create_subscription(UInt32,"studentID",self.recv_studentID_callback,10)
+
+        self.declare_parameter("pub_timer_period",1)
 
     def timer_callback(self):
         msg = String()
@@ -21,6 +23,11 @@ class TeacherNode(Node):
         self.pub_hw.publish(msg)
         self.get_logger().info('同学们，我发布了作业："%s"' % msg.data)
         self.i += 1
+
+        # 回调之后更新回调周期
+        timer_period = self.get_parameter('pub_timer_period').get_parameter_value().integer_value
+        # 更新回调周期
+        self.timer.timer_period_ns = timer_period * (1000*1000*1000)
 
     def recv_studentID_callback(self,studengID):
         self.get_logger().info('学号：%d已经收到了作业' % studengID.data)
