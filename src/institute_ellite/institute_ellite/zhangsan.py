58113329f4a28cf4ba5b7aad033536e3b4cb3ff6
zhanglei <zhanglei.20082006@163.com>
2025-03-03 12:40:03 +0800

    topic commit

--- src/institute_ellite/institute_ellite/zhangsan.py
+++ src/institute_ellite/institute_ellite/zhangsan.py
@@ -1,10 +1,29 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String
+from std_msgs.msg import UInt32
 
 class TeacherNode(Node):
     def __init__(self, name):
         super().__init__(name)
         self.get_logger().info("我是%s,是一名老师！" %name)
+
+        self.pub_hw = self.create_publisher(String,"homework",10)
+        self.i = 1
+        self.timer_period = 2
+        self.timer = self.create_timer(self.timer_period,self.timer_callback)
+
+        self.substudentID = self.create_subscription(UInt32,"studentID",self.recv_studentID_callback,10)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = '第%d次作业已发布,请查收。' % (self.i)
+        self.pub_hw.publish(msg)
+        self.get_logger().info('同学们，我发布了作业："%s"' % msg.data)
+        self.i += 1
+
+    def recv_studentID_callback(self,studengID):
+        self.get_logger().info('学号：%d已经收到了作业' % studengID.data)
 
 
 def main(args=None):
--- src/institute_mechanical/src/lisi.cpp
+++ src/institute_mechanical/src/lisi.cpp
@@ -1,5 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/u_int32.hpp"
 
+using std::placeholders::_1;
 
 class StudentNode:public rclcpp::Node
 {
@@ -8,9 +11,24 @@ public:
     {
         RCLCPP_INFO(this->get_logger(),"我是学生%s",name.c_str());
 
+        sub_hw = this->create_subscription<std_msgs::msg::String>("homework",10,std::bind(&StudentNode::topic_callback,this,_1));
+        pub_No = this->create_publisher<std_msgs::msg::UInt32>("studentID",10);
     }
 
 private:
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_hw;
+    rclcpp::Publisher<std_msgs::msg::UInt32>::SharedPtr pub_No;
+
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+    {
+        RCLCPP_INFO(this->get_logger(),"收到：'%s'",msg->data.c_str());
+
+        std_msgs::msg::UInt32 studengid;
+        studengid.data = 10;
+        pub_No->publish(studengid);
+        RCLCPP_INFO(this->get_logger(),"学号：%d,收到：'%s'",studengid.data,msg->data.c_str());
+
+    };
 
     
 };
